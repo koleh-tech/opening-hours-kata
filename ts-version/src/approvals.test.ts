68aa997e3994aa13167a83e4c75f59d16c63a653
koleh <kolehd@gmail.com>
2025-06-14 18:18:34 +0930

    B rm mocha

--- ts-version/src/approvals.test.ts
+++ ts-version/src/approvals.test.ts
@@ -1,6 +1,10 @@
-require("approvals").mocha()
-
+var approvals = require("approvals")
 describe("When running some tests", function () {
+    beforeEach(function () {
+        this.setOptions({
+            reporters: ["meld"],
+        })
+    })
     it("should be able to use Approvals", function () {
         var data = "Hello World!"
         this.verify(data, { reporters: ["meld"] }) // or this.verifyAsJSON(data)
