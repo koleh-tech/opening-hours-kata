28ab9d563b99837a0117c9a18948f2feb0a0b5d1
koleh.david <koleh.david@aerometrex.com.au>
2025-06-16 18:12:14 +0930

    t introduce new example

--- ts-version/src/approvals.test.ts
+++ ts-version/src/approvals.test.ts
@@ -29,8 +29,11 @@ describe("OpeningHours", function () {
         const days = ["mon", "wed", "fri"]
         const openHours = new OpeningHours(days, "08:00", "16:00")
         const header = `CONFIG:\nDAYS OPEN: ${days}\nHOURS OPEN: ${"08:00"} - ${"16:00"}\n`
-        const inputDays = [`2016-05-16T12:22:11.824Z`]
-        const result = inputDays
+        const inputHours = [
+            ...[`2016-05-16T12:22:11.824Z`],
+            ...[`2016-05-16T12:22:11.824Z`],
+        ]
+        const result = inputHours
             .map((input) => {
                 const openText = openHours.isOpenOn(input) ? "OPEN" : "CLOSED"
                 return `${openHours.dayNameFor(input)} => ${openText}`
@@ -39,13 +42,3 @@ describe("OpeningHours", function () {
         approvals.verify(__dirname, "open-on-hours", header + result)
     })
 })
-
-// def test_is_open_on(shop_hours: OpeningHours):
-//     results = {
-//         "wednesday": shop_hours.is_open_on("2016-05-11T12:22:11.824Z"),
-//         "wednesday_after_hours": shop_hours.is_open_on("2016-05-11T16:01:00.000Z"),
-//         "thursday": shop_hours.is_open_on("2016-05-12T12:22:11.824Z"),
-//     }
-//
-//     verify(f"{header_for_hours(shop_hours)}{results}")
-//
