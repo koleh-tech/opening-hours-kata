6860c9f93681a6e492ac4dd7fa6a313518042ceb
koleh.david <koleh.david@aerometrex.com.au>
2025-06-16 17:55:27 +0930

    r one of many

--- ts-version/src/approvals.test.ts
+++ ts-version/src/approvals.test.ts
@@ -18,19 +18,18 @@ describe("OpeningHours", function () {
             "2016-05-09T12:22:11.824Z",
             "2016-05-10T12:22:11.824Z",
             "2016-05-11T12:22:11.824Z",
-            `2016-05-12T12:22:11.824Z`,
+            `2016-05-${12}T12:22:11.824Z`,
         ]
+        const fourth = openHours.dayNameFor(inputs[3])
+        // __AUTO_GENERATED_PRINT_VAR_START__
+        console.log("(anon)#(anon) fourth: %s", fourth) // __AUTO_GENERATED_PRINT_VAR_END__
         const result = inputs
             .map((input) => {
                 const openText = openHours.isOpenOn(input) ? "OPEN" : "CLOSED"
                 return `${openHours.dayNameFor(input)} => ${openText}`
             })
             .join("\n")
-        approvals.verify(
-            __dirname,
-            "open-on-days",
-            `2016-05-12T12:22:11.824Z` + header + result,
-        )
+        approvals.verify(__dirname, "open-on-days", header + result)
     })
 })
 
