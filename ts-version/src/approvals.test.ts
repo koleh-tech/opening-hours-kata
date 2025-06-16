739905af54e29fa89e5ad783c2b820d724d77690
koleh.david <koleh.david@aerometrex.com.au>
2025-06-16 18:05:05 +0930

    t start testing hours

--- ts-version/src/approvals.test.ts
+++ ts-version/src/approvals.test.ts
@@ -24,6 +24,24 @@ describe("OpeningHours", function () {
             .join("\n")
         approvals.verify(__dirname, "open-on-days", header + result)
     })
+
+    it("is open on hours", function () {
+        const days = ["mon", "wed", "fri"]
+        const openHours = new OpeningHours(days, "08:00", "16:00")
+        const header = `CONFIG:\nDAYS OPEN: ${days}\nHOURS OPEN: ${"08:00"} - ${"16:00"}\n`
+        const inputDays = [
+            ...[16, 17, 18, 19].map(
+                (dayNum) => `2016-05-${dayNum}T12:22:11.824Z`,
+            ),
+        ]
+        const result = inputDays
+            .map((input) => {
+                const openText = openHours.isOpenOn(input) ? "OPEN" : "CLOSED"
+                return `${openHours.dayNameFor(input)} => ${openText}`
+            })
+            .join("\n")
+        approvals.verify(__dirname, "open-on-hours", header + result)
+    })
 })
 
 // def test_is_open_on(shop_hours: OpeningHours):
