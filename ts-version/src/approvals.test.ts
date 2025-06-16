60d060f3a4cca52ab7ed4811820e057e7bd7c643
koleh.david <koleh.david@aerometrex.com.au>
2025-06-16 18:04:07 +0930

    T added hours open to header

--- ts-version/src/approvals.test.ts
+++ ts-version/src/approvals.test.ts
@@ -13,16 +13,13 @@ describe("OpeningHours", function () {
             "08:00",
             "16:00",
         )
-        const header = `CONFIG:\nDAYS OPEN: ${["mon", "wed", "fri"]}\n`
-        const inputs = [
+        const header = `CONFIG:\nDAYS OPEN: ${["mon", "wed", "fri"]}\nHOURS OPEN: ${"08:00"} - ${"16:00"}\n`
+        const inputDays = [
             ...[16, 17, 18, 19].map(
                 (dayNum) => `2016-05-${dayNum}T12:22:11.824Z`,
             ),
         ]
-        const fourth = openHours.dayNameFor(inputs[3])
-        // __AUTO_GENERATED_PRINT_VAR_START__
-        console.log("(anon)#(anon) fourth: %s", fourth) // __AUTO_GENERATED_PRINT_VAR_END__
-        const result = inputs
+        const result = inputDays
             .map((input) => {
                 const openText = openHours.isOpenOn(input) ? "OPEN" : "CLOSED"
                 return `${openHours.dayNameFor(input)} => ${openText}`
