e171f3c697d4165ef0997efe62455f1cbc1c22fe
koleh.david <koleh.david@aerometrex.com.au>
2025-06-17 12:50:01 +0930

    r fixed time

--- ts-version/src/OpeningHours.test.ts
+++ ts-version/src/OpeningHours.test.ts
@@ -1,10 +1,19 @@
-import { expect, test } from "vitest"
+import { describe, expect, it } from "vitest"
 import { Period, Datetime, OpeningHours } from "./OpeningHours"
 
-test("adds 1 + 2 to equal 3", () => {
-    const toApprove = new OpeningHours(
-        ["mon", "wed", "fri"],
-        new Period("08:00", "16:00"),
-    ).isOpenOn(new Datetime("2016-05-11T12:22:11.824Z"))
-    expect(toApprove).toBe(false)
+describe("OpeningHours", () => {
+    it("Can be open on all days", () => {
+        const input = new Datetime("2016-05-11T06:29:11.824Z")
+        expect(input.dayName()).toBe("Wed")
+
+        const openingHours = new OpeningHours(
+            ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+            new Period("08:00", "16:00"),
+        )
+        expect(openingHours.isOpenOn(input)).toBe(true)
+
+        expect(
+            new Datetime(openingHours.nextOpeningDate(input)).dayName(),
+        ).toBe("Thu")
+    })
 })
