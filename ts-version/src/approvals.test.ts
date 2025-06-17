707d335ede6e08e1191ab9ac7805e5cd09ee7275
koleh.david <koleh.david@aerometrex.com.au>
2025-06-17 12:52:45 +0930

    r datetime conversion on input

--- ts-version/src/approvals.test.ts
+++ ts-version/src/approvals.test.ts
@@ -10,16 +10,17 @@ describe("OpeningHours", function () {
     const days = ["Mon", "Wed", "Fri"]
     const openHours = new OpeningHours(days, new Period("08:00", "16:00"))
     const header = `CONFIG:\nDAYS OPEN: ${days}\nHOURS OPEN: ${"08:00"} - ${"16:00"}\n`
+
     it("is open on days", function () {
         const inputDays = [
             ...[16, 17, 18, 19].map(
-                (dayNum) => `2016-05-${dayNum}T06:29:11.824Z`,
+                (dayNum) => new Datetime(`2016-05-${dayNum}T06:29:11.824Z`),
             ),
         ]
         const result = inputDays
             .map((input) => {
-                const inputText = new Datetime(input).dayName()
-                return `${inputText} => ${openHours.isOpenOn(new Datetime(input)) ? "OPEN" : "CLOSED"}`
+                const thi = input
+                return `${thi.dayName()} => ${openHours.isOpenOn(thi) ? "OPEN" : "CLOSED"}`
             })
             .join("\n")
         approvals.verify(__dirname, "open-on-days", header + result)
